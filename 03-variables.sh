fbfc0a048ee65f02946af2a91853b01149488d08
ysanjeevkumar <ysanjeevkumar@gmail.com>
2026-01-29 22:21:26 +0530

    action

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=Ram
+person2=Laxman
+echo "$person1:: Hello $person2, how are you?"
+echo "$person2:: Hi $person1, Iam fine what are you doing?"
+echo "$person1:: Doing fine. What are you learning $person2?"
+echo "$person2:: Iam learning Shell. What about you"
